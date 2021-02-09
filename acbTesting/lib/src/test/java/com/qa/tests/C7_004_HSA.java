66405d8f596ccf60794696ac8f9234a890ed1aa6
Stuzoo <brocksd@gmail.com>
2021-02-09 15:58:45 +0400

    Update 004

--- acbTesting/lib/src/test/java/com/qa/tests/C7_004_HSA.java
+++ acbTesting/lib/src/test/java/com/qa/tests/C7_004_HSA.java
@@ -10,6 +10,7 @@ import java.util.Map;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -25,6 +26,7 @@ import com.aspirehr.pagefactory.EnrollmentInstPage;
 import com.aspirehr.pagefactory.FSADeptCarePage;
 import com.aspirehr.pagefactory.FSAEmpHlthCarePage;
 import com.aspirehr.pagefactory.FlexCreditPage;
+import com.aspirehr.pagefactory.HSASavingPlanPage;
 import com.aspirehr.pagefactory.MassMutalPSPage;
 import com.aspirehr.pagefactory.MasterListPage;
 import com.aspirehr.pagefactory.MedicalPage;
@@ -64,7 +66,7 @@ public class C7_004_HSA {
 
 	private static FSAEmpHlthCarePage fsaEmpHlthCarePage;
 	private static FSADeptCarePage fsaDeptCareSpendingPage;
-
+	private static HSASavingPlanPage hsaSavingPlanPage;
 	private static Savings401KPlanPage savings401kPage;
 	private static Savings401KCatchupPage savings401kCatchupPage;
 	private static MiscHealthClubPage miscHealthClubPage;
@@ -75,16 +77,18 @@ public class C7_004_HSA {
 
 	@BeforeClass
 	public static void setup() {
-		// System.setProperty("webdriver.gecko.driver",
-		// "c:/Gecko/geckodriver.exe");
-		// System.setProperty("webdriver.chrome.driver",
-		// "C:/ChromeDriver/chromedriver.exe");
 		FirefoxProfile ffProfile = new FirefoxProfile();
-		ffProfile.setPreference("network.automatic-ntlm-auth.trusted-uris", "ahr-e68.erpsolutions.local:8000");
+		ffProfile.setPreference("network.automatic-ntlm-auth.trusted-uris", "10.20.4.216:8443");
 		// driver = new FirefoxDriver(ffProfile);
 		// driver = new ChromeDriver();
 		System.setProperty("webdriver.chrome.driver", "/Users/stu/Drivers/chromedriver");
-		WebDriver driver = new ChromeDriver();
+		
+		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("--window-size=1920,1200");
+		options.setAcceptInsecureCerts(true);
+		
+		WebDriver driver = new ChromeDriver(options);
+		
 		// driver = new InternetExplorerDriver();
 		// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		// driver.manage().window().maximize();
@@ -94,7 +98,7 @@ public class C7_004_HSA {
 		String URL = "10.20.4.216:8443/sap/bc/ui5_ui5/ahrclben/cloud/index.html";
 		// String URL =
 		// "ahr-e68demo.erpsolutions.local:8000/sap/bc/ui5_ui5/ahrclben/cloud/index.html";
-		String uid = "c704";
+		String uid = "c702";
 		String pwd = "c7testing";
 
 		// this is for IE
@@ -126,6 +130,7 @@ public class C7_004_HSA {
 		suppLifePage = new SuppLifePage(driver);
 		fsaEmpHlthCarePage = new FSAEmpHlthCarePage(driver);
 		fsaDeptCareSpendingPage = new FSADeptCarePage(driver);
+		hsaSavingPlanPage = new HSASavingPlanPage(driver);
 
 		savings401kPage = new Savings401KPlanPage(driver);
 		savings401kCatchupPage = new Savings401KCatchupPage(driver);
@@ -156,273 +161,24 @@ public class C7_004_HSA {
 	}
 
 	@Test(priority = 1, dependsOnMethods = { "test1_MasterListPage_Loaded" })
-	public void test01_CreditDefaulting() {
-		// Navigate to New Hire Enrollment--> Personal Data and verify Data
-		masterListPage.clickMasterListItem("New Hire Enrollment"); // E68
-		masterListPage.clickMasterListItem("New Hire Enrollment");
-		// masterListPage.clickMasterListItem("Complete New Hire"); // Q68
+	public void test01_HSAamounts() {
+		
+		masterListPage.clickMasterListItem("Complete Anytime Enrollment"); 
+		
 		try {
+			Thread.sleep(5000);
+			Thread.sleep(5000);
+			masterListPage.clickMasterListItem("Dependent Care Spending Plan");
 			Thread.sleep(3000);
+			
+			hsaSavingPlanPage.enterRegularContribution("9999999"); ;// sapMListTblSelCol
+			hsaSavingPlanPage.savePlan();
 
-			masterListPage.clickMasterListItem("Personal Data");
-			Thread.sleep(3000);
-			String detPageTitle = persDataPage.getPageTitle();
-			String empName = persDataPage.getName();
-			String dob = persDataPage.getDOB();
-			Map<String, String> permAddress = persDataPage.getPermAddress();
-			Map<String, String> mailAddress = persDataPage.getMailingAddress();
-			assertTrue(detPageTitle.equals("PERSONAL DATA"));
-			assertTrue(empName.equals("Federer Roger"));
-			assertTrue(dob.equals("07/24/1956"));
-			assertTrue(permAddress.get("state").equals(""));
-			assertTrue(permAddress.get("zip").equals("76039"));
-
-			assertTrue(mailAddress.get("state").equals("Texas"));
+			
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
-	// @Test(priority = 2, dependsOnMethods = { "test20_NewHire_PersonalData" })
-	// public void test21_EnrolledPlan() {
-	// int enrolledPlanCount = masterListPage.getEnrolledPlanCount();
-	// assertTrue(enrolledPlanCount == 5);
-	// }
-	//
-	// @Test(priority = 3, dependsOnMethods = { "test20_NewHire_PersonalData" })
-	// public void test22_PendingPlan() {
-	// int pendingPlanCount = masterListPage.getPendingPlanCount();
-	// assertTrue(pendingPlanCount == 0);
-	// }
-	//
-	// @Test(priority = 4, dependsOnMethods = { "test20_NewHire_PersonalData" })
-	// public void test23_AvailablePlanToEnroll() {
-	// int availablePlanCount = masterListPage.getAvailablePlanToEnrollCount();
-	// assertTrue(availablePlanCount == 6);
-	// }
-	//
-	// @Test(priority = 5, dependsOnMethods = { "test20_NewHire_PersonalData" })
-	// public void test24_verifyTotalCostBeforeEnrollment() {
-	// // masterListPage.clickMasterListItem("Review and Enroll");
-	// // masterListPage.clickMasterListItem("Review and Enroll");
-	// // try {
-	// // Thread.sleep(3000);
-	// // } catch (InterruptedException e) {
-	// // // TODO Auto-generated catch block
-	// // e.printStackTrace();
-	// // }
-	// // float totalCost = reviewnEnrollPage.getTotalCost();
-	// // System.out.println("totalCost::" + totalCost);
-	// // float tmp = (float) -25.89;
-	// // assertTrue(totalCost == tmp);
-	// test71_ReviewTotalCostAddition();
-	// }
-
-	// @Test(priority = 6, dependsOnMethods = { "test20_NewHire_PersonalData" })
-	// public void test30_DeptListData() {
-	// // masterListPage.clickMasterListItem("New Hire Enrollment");
-	// try {
-	// masterListPage.clickMasterListItem("Dependents and Beneficiaries");
-	// Thread.sleep(3000);
-	// ArrayList<String> deptList = deptDataPage.getDeptList();
-	// System.out.println("Dept List:" + deptList.toString());
-	// Boolean foundSpouse = false;
-	// for (String dept : deptList) {
-	// if (dept.equals("Jennifer Federer")) {
-	// foundSpouse = true;
-	// break;
-	// }
-	// }
-	// assertTrue(foundSpouse);
-	// } catch (InterruptedException e) {
-	// e.printStackTrace();
-	// }
-	// }
-
-	// @Test( priority=7 , dependsOnMethods = { "test20_NewHire_PersonalData" })
-	// public void test31_FlexCredits() {
-	// try {
-	// masterListPage.clickMasterListItem("Flex Credits");
-	// Thread.sleep(3000);
-	// ArrayList<String> flexCreditDts = flexCreditPage.getFlexCreditDetails();
-	// System.out.println("Flex Credits:" + flexCreditDts.toString());
-	// Boolean foundFlxCreditAmt = false;
-	// for (String flexCredit : flexCreditDts) {
-	// if (flexCredit.equals("-100.00 USD")) {
-	// foundFlxCreditAmt = true;
-	// break;
-	// }
-	// }
-	// assertTrue(foundFlxCreditAmt);
-	//
-	// // //Begin of Review total cost test reuse
-	// //
-	// // // Check the running Header Total cost match with the sum up of
-	// // // individual EE cost.
-	// // float flexCreditPageTotalCost =
-	// // flexCreditPage.getTotalCostFromHeader();//
-	// // test80_RunningTotalCost(flexCreditPageTotalCost);
-	// //
-	// // masterListPage.clickMasterListItem("Flex Credits");
-	// // masterListPage.clickMasterListItem("Flex Credits");
-	// //
-	// // // End of Review total cost test reuse
-	//
-	// // Check the running Header Total cost match with the Detail Page
-	// // NewTotalCost variable after the previous plan selection.
-	// float currentTotalCost = flexCreditPage.getTotalCostFromHeader();
-	// assertTrue(currentTotalCost == DetailPage.newTotalCost); // Initial
-	// // Detail
-	// // Page
-	// // newTotalCost
-	// // will
-	// // be
-	// // set
-	// // from
-	// // ReviewnEnroll
-	// // page.
-	//
-	// DetailPage.currentTotalCost = flexCreditPage.getTotalCostFromHeader();
-	// DetailPage.currentPlanCost = flexCreditPage.getPlanCostFromHeader();
-	// System.out.println(
-	// "Curr Tot Cost:" + DetailPage.currentTotalCost + "//Curr Plan Cost:" +
-	// DetailPage.currentPlanCost);
-	//
-	// } catch (InterruptedException e) {
-	// e.printStackTrace();
-	// }
-	// }
-	//
-	// @Test( priority=8 , dependsOnMethods = { "test31_FlexCredits" })
-	// public void test32_FlexCreditEditnSave() {
-	// try {
-	// flexCreditPage.clickUpdate();// sapMListTblSelCol
-	// flexCreditPage.selectPlan(1);
-	// DetailPage.newPlanCost = flexCreditPage.getSelectedPlanCost(1);
-	// flexCreditPage.savePlan();
-	// System.out.println("Pagetitle::" + medicalPage.getPageTitle());
-	// Thread.sleep(3000);
-	// assertTrue(medicalPage.getPageTitle().equals("MEDICAL"));
-	//
-	// // Begin of running total cost
-	// float newTotalCost = DetailPage.currentTotalCost -
-	// DetailPage.currentPlanCost + DetailPage.newPlanCost;
-	// DetailPage.newTotalCost = newTotalCost;
-	// // End of running total cost
-	//
-	// } catch (InterruptedException e) {
-	// e.printStackTrace();
-	// }
-	// }
-
-//	@Test(priority = 9, dependsOnMethods = { "test20_NewHire_PersonalData" })
-//	public void test33_PlanDetails() {
-//		try {
-//			masterListPage.clickMasterListItem("Medical");
-//			masterListPage.clickMasterListItem("Medical");
-//			Thread.sleep(3000);
-//			ArrayList<String> planDetails = medicalPage.getPlanDetails();
-//			Boolean foundPlanOption = false;
-//			for (String planDetail : planDetails) {
-//				if (planDetail.equals("POSPOption")) {
-//					foundPlanOption = true;
-//					break;
-//				}
-//			}
-//			assertTrue(foundPlanOption);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//	}
-
-//	@Test(priority = 10, dependsOnMethods = { "test33_PlanDetails" })
-//	public void test34_EnrollPlanPending() {
-//		try {
-//			medicalPage.clickUpdate();// sapMListTblSelCol
-//			medicalPage.selectPlan(1);
-//			DetailPage.newPlanCost = medicalPage.getSelectedPlanCost(1);
-//			System.out.println("New Plan Cost::" + DetailPage.newPlanCost);
-//			medicalPage.savePlan();
-//			Thread.sleep(3000);
-//			assertTrue(dentalPage.getPageTitle().equals("DENTAL"));
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//	}
-
-//	@Test(priority = 11, dependsOnMethods = { "test34_EnrollPlanPending" })
-//	public void test35_PlanUndoPending() {
-//		try {
-//			masterListPage.clickMasterListItem("Medical");
-//			masterListPage.clickMasterListItem("Medical");
-//			assertTrue(medicalPage.isUndoPendingBtnVisible());
-//			if (medicalPage.isUndoPendingBtnVisible()) {
-//				medicalPage.undoPendingPlan();
-//				Thread.sleep(3000);
-//				String undoPendingPlanMessage = medicalPage.getUndoPendingMessage();
-//				medicalPage.closeMessageBox();
-//				Thread.sleep(2000);
-//				assertTrue(undoPendingPlanMessage.equals("Pending record removed"));
-//			}
-//		} catch (Exception e) {
-//
-//		}
-//	}
 	
-//	@Test(priority = 12, dependsOnMethods = { "test20_NewHire_PersonalData" })
-//	public void test36_PlanDetails() {
-//		try {
-//			masterListPage.clickMasterListItem("Medical");
-//			masterListPage.clickMasterListItem("Medical");
-//			Thread.sleep(3000);
-//			ArrayList<String> planDetails = medicalPage.getPlanDetails();
-//			Boolean foundPlanOption = false;
-//			for (String planDetail : planDetails) {
-//				if (planDetail.equals("POSPOption")) {
-//					foundPlanOption = true;
-//					break;
-//				}
-//			}
-//			assertTrue(foundPlanOption);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//	}
-
-//	@Test(priority = 13, dependsOnMethods = { "test33_PlanDetails" })
-//	public void test37_EnrollPlanPending() {
-//		try {
-//			medicalPage.clickUpdate();// sapMListTblSelCol
-//			medicalPage.selectPlan(1);
-//			DetailPage.newPlanCost = medicalPage.getSelectedPlanCost(1);			
-//			medicalPage.savePlan();
-//			Thread.sleep(3000);
-//			assertTrue(dentalPage.getPageTitle().equals("DENTAL"));			
-//
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//	}
-
-//	@Test(priority = 14, dependsOnMethods = { "test34_EnrollPlanPending" })
-//	public void test38_PlanUndoPending() {
-//		try {
-//			masterListPage.clickMasterListItem("Medical");
-//			masterListPage.clickMasterListItem("Medical");
-//
-//			assertTrue(medicalPage.isUndoPendingBtnVisible());
-//			if (medicalPage.isUndoPendingBtnVisible()) {
-//				medicalPage.undoPendingPlan();
-//				Thread.sleep(3000);
-//				String undoPendingPlanMessage = medicalPage.getUndoPendingMessage();
-//				medicalPage.closeMessageBox();
-//				Thread.sleep(2000);
-//				assertTrue(undoPendingPlanMessage.equals("Pending record removed"));
-//			}
-//		} catch (Exception e) {
-//
-//		}
-//	}
-
 }
